a14e264f8b5e61611b881538b6fd620003245bf9
tamanugi <tamanugi@gmail.com>
2017-10-08 18:34:51 +0900

    Excelからコピペされた表をJSONに変換する処理を仮実装

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cheerio from 'cheerio'
 
 class App extends Component {
+  
+
+  changeTextare = (e) => {
+
+    console.log(e.clipboardData) 
+    let html = e.clipboardData.getData('text/html')
+    if (/<table/i.test(html)) {
+      const $ = cheerio.load(html),
+      r = $("table");
+      let trs = r.children().children("tr");
+      console.log(r.html())
+
+      let json = []
+      let headers = []
+      trs.each((i, tr) => {
+        // 1行目はヘッダーとして処理する
+        if(i === 0){
+          $(tr).children("td").map((ii, td) => {
+            headers.push($(td).text())
+          })
+          return 
+        }
+
+        let obj = {}
+        $(tr).children("td").map((ii, td) => {
+          let header = headers[ii]
+          obj[header] = $(td).text()
+        })
+
+        json.push(obj)
+
+      })
+      console.log(JSON.stringify(json))
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +49,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <textarea onPaste={this.changeTextare}></textarea>
       </div>
     );
   }
